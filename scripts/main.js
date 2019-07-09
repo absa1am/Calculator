ba66ef82dd1c2c024cc418fe6b8602abcaa43e15
Abdus Salam <salaam.mbstu@gmail.com>
2019-07-09 23:42:27 +0600

    Calculator manipulator

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let answer = "";
+let currentOperation = "";
+
+function numberPressed(number) {
+  currentOperation += number;
+  document.getElementById('display').value = currentOperation;
+}
+
+function operatorPressed(operator) {
+  if(currentOperation == "") currentOperation = answer;
+  if(currentOperation[0] == '*' || currentOperation[0] == '/' || eval(currentOperation) == "Infinity") document.getElementById('display').value = "E";
+  else if(operator === '=') {
+    currentOperation = eval(currentOperation ? currentOperation:"0");
+    document.getElementById('display').value = eval(currentOperation);
+    answer = eval(currentOperation);
+    currentOperation = "";
+  } else if(operator == "Ans") {
+    document.getElementById('display').value = answer;
+  } else {
+    currentOperation += operator;
+    document.getElementById('display').value = currentOperation;
+  }
+}
+
+function clearPressed() {
+  currentOperation = "";
+  document.getElementById('display').value = "0";
+}
+
+function deletePressed() {
+  currentOperation = currentOperation.slice(0, -1);
+  document.getElementById('display').value = currentOperation;
+}
